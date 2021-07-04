b1eb5819269040c0ed7376e41b92f8ae8ba85a75
ガイア <82070451+ToshitakeTakahashi@users.noreply.github.com>
2021-07-04 19:28:39 +0000

    I wrote code about Problem2

--- Exercise_10_Problem_1_3.py
+++ Exercise_10_Problem_1_3.py
@@ -72,8 +72,9 @@ print("Geocoded output is stored in this file:", out_fp)
  
 
 # YOUR CODE HERE 6 to create a new column
-
+geodata['buffer'] = None
 # YOUR CODE HERE 7 to set buffer column
+geodata['buffer'] = geodata.buffer(distance=1500)
 
 #TEST CODE
 print(geodata.head())
@@ -91,7 +92,7 @@ print(round(gpd.GeoSeries(geodata["buffer"]).area / 1000000))
 # - Replace the values in `geometry` column with the values of `buffer` column:
 
 # YOUR CODE HERE 8 to replace the values in geometry
-
+geodata['geometry'] = geodata['buffer']
 #TEST CODE
 print(geodata.head())
 
